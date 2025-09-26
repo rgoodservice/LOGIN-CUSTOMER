02e6138bad40ed3d0318817ccb2f1e4e3b02c28d
rgoodservice <rgoodservice84@gmail.com>
2025-09-26 15:42:59 +0700

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
 // supabase-config.js
 // TODO: put your real values here (from Supabase Settings → API)
-export const SUPABASE_URL = "https://YOUR-PROJECT.supabase.co";
-export const SUPABASE_ANON_KEY = "YOUR_PUBLIC_ANON_KEY";
-export const ADMIN_EMAIL = "admin@example.com"; // optional
+export const SUPABASE_URL = "https://qmugpalgcktfwixqepti.supabase.co";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFtdWdwYWxnY2t0ZndpeHFlcHRpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg4NzI3NjcsImV4cCI6MjA3NDQ0ODc2N30.cIOCbJAr8_Gbg8d5IxpTQZKVRgl1ETN1hyBgL2_LveA";
+export const ADMIN_EMAIL = "rgoodservice84@gmail.com"; // optional
